6815bd6cf4e02dd7b5290070447eb8ceab063bc7
Alan Moran <bonzofenix@gmail.com>
2019-03-20 07:17:17 -0700

    Adds pin resources task
    Signed-off-by: Joe Goller <joe.goller@aon.com>

---
+++ ci/scripts/pin-resources.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+exec >&2
+set -e
+
+source pipeline/ci/scripts/common.sh
+
+function authenticate_concourse(){
+  fly -t concourse login -k -u $CONCOURSE_USERNAME -p $CONCOURSE_PASSWORD -c $CONCOURSE_TARGET
+}
+
+function pin_versions(){
+  while read pin; do
+    resource_name=$(echo $pin | cut -d':' -f1)
+    version_regex=$(echo $pin | cut -d':' -f2)
+
+    versions_response="$(fly -t concourse curl /api/v1/teams/$CONCOURSE_TEAM/pipelines/$PIPELINE_NAME/resources/$resource_name/versions)"
+
+    version_id=$(echo $version_response | jq -r ".[] | select(.version.product_version | contains('$version_regex')) | .id")
+
+    fly -t concourse curl /api/v1/teams/$CONCOURSE_TEAM/pipelines/$PIPELINE_NAME/resources/$resource_name/versions/$version_id/pin
+  done < $PINS_FILE
+}
+
+check_if_exists "PIPELINE_NAME is empty" $PIPELINE_NAME
+check_if_exists "PINS_FILE is empty" $PINS_FILE
+check_if_exists "CONCOURSE_TEAM is empty" $CONCOURSE_TEAM
+check_if_exists "CONCOURSE_TARGET is empty" $CONCOURSE_TARGET
+check_if_exists "CONCOURSE_USERNAME is empty" $CONCOURSE_USERNAME
+check_if_exists "CONCOURSE_PASSWORD is empty" $CONCOURSE_PASSWORD
+
+authenticate_concourse
+pin_versions

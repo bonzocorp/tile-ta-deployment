fdc0343427126a9fb2808f733e7d9798a596e067
Alan Moran <alan.moran1@aon.com>
2020-03-05 17:43:46 +0000

    Fix issue with csplit command when adding custom certs
    Signed-off-by: Joe Goller <joe.goller@aon.com>

--- ci/scripts/common.sh
+++ ci/scripts/common.sh
@@ -25,7 +25,7 @@ mkdir -p $OUTPUT
 function load_custom_ca_certs(){
   if [[ ! -z "$CUSTOM_CERTS" ]] ; then
     echo -e "$CUSTOM_CERTS" > custom_certs.crt
-    csplit -k -f /etc/ssl/certs/ custom_certs.crt '/END CERTIFICATE/+1' {10}
+    csplit -k -f /etc/ssl/certs/ -b "%04d.crt" custom_certs.crt '/END CERTIFICATE/+1' '{*}'
   fi
 
   update-ca-certificates
